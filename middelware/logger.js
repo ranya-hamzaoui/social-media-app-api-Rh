6e91bb092d55bfce9468c9881b0b6cadb85ce50b
ranya hamzaoui <ranya.hamzaoui91@gmail.com>
2024-10-05 01:29:07 +0200

    fix error log file2

--- middelware/logger.js
+++ middelware/logger.js
@@ -1,71 +1,71 @@
-const { createLogger, format, transports } = require("winston");
-var winston = require("winston");
-require("winston-daily-rotate-file");
-let path = require("path");
-let config = require("../config/server");
+// const { createLogger, format, transports } = require("winston");
+// var winston = require("winston");
+// require("winston-daily-rotate-file");
+// let path = require("path");
+// let config = require("../config/server");
 
-const logger = createLogger({
-  level: config.debugLogging ? "debug" : "info",
-  format: format.combine(
-    format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss"
-    }),
-    format.errors({
-      stack: true
-    }),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: {
-    service: "DPA-WEB-SERVER"
-  },
-  transports: [
-    new winston.transports.DailyRotateFile({
-      name: "file#info",
-      level: "info",
-      //maxSize: '20m',
-      maxFiles: "1d",
-      filename: path.join(config.logsDir, "info.log"),
-      datePattern: "YYYY-MM-DD",
-      auditFile: path.join(config.logsDir, "application-audit.json"),
-      zippedArchive: true
-    }),
-    new winston.transports.DailyRotateFile({
-      name: "file#error",
-      level: "error",
-      //maxSize: '20m',
-      maxFiles: "1d",
-      filename: path.join(config.logsDir, "error.log"),
-      datePattern: "YYYY-MM-DD",
-      zippedArchive: true,
-      auditFile: path.join(config.logsDir, "application-audit.json"),
-      handleExceptions: true
-    })
-  ]
-});
+// const logger = createLogger({
+//   level: config.debugLogging ? "debug" : "info",
+//   format: format.combine(
+//     format.timestamp({
+//       format: "YYYY-MM-DD HH:mm:ss"
+//     }),
+//     format.errors({
+//       stack: true
+//     }),
+//     format.splat(),
+//     format.json()
+//   ),
+//   defaultMeta: {
+//     service: "DPA-WEB-SERVER"
+//   },
+//   transports: [
+//     new winston.transports.DailyRotateFile({
+//       name: "file#info",
+//       level: "info",
+//       //maxSize: '20m',
+//       maxFiles: "1d",
+//       filename: path.join(config.logsDir, "info.log"),
+//       datePattern: "YYYY-MM-DD",
+//       auditFile: path.join(config.logsDir, "application-audit.json"),
+//       zippedArchive: true
+//     }),
+//     new winston.transports.DailyRotateFile({
+//       name: "file#error",
+//       level: "error",
+//       //maxSize: '20m',
+//       maxFiles: "1d",
+//       filename: path.join(config.logsDir, "error.log"),
+//       datePattern: "YYYY-MM-DD",
+//       zippedArchive: true,
+//       auditFile: path.join(config.logsDir, "application-audit.json"),
+//       handleExceptions: true
+//     })
+//   ]
+// });
 
-if (config.nodeEnv !== "production") {
-  logger.add(
-    new transports.Console({
-      level: "debug",
-      stderrLevels: ["error", "debug", "info"],
-      consoleWarnLevels: ["warn"],
-      handleExceptions: true,
-      format: format.combine(
-        format.colorize(),
-        format.simple(),
-        format.printf(msg => `${msg.timestamp} - ${msg.level}: ${msg.message}`)
-      )
-    })
-  );
-  logger.debug("Logging initialized at debug level");
-}
+// if (config.nodeEnv !== "production") {
+//   logger.add(
+//     new transports.Console({
+//       level: "debug",
+//       stderrLevels: ["error", "debug", "info"],
+//       consoleWarnLevels: ["warn"],
+//       handleExceptions: true,
+//       format: format.combine(
+//         format.colorize(),
+//         format.simple(),
+//         format.printf(msg => `${msg.timestamp} - ${msg.level}: ${msg.message}`)
+//       )
+//     })
+//   );
+//   logger.debug("Logging initialized at debug level");
+// }
 
-logger.stream = {
+// logger.stream = {
   
- write: function(message) {
-    logger.info(message.trim());
-  }
+//  write: function(message) {
+//     logger.info(message.trim());
+//   }
 
-};
-module.exports = logger;
+// };
+// module.exports = logger;
