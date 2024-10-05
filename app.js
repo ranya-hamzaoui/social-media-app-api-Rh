39507bf41365088b9f24c253e16c57b8acddfd08
ranya hamzaoui <ranya.hamzaoui91@gmail.com>
2024-10-05 15:47:03 +0200

    change port configuration

--- app.js
+++ app.js
@@ -78,4 +78,9 @@ app.use(function(req, res, next) {
   next(boom.notFound()); 
 });
 app.use(ErrorHandle);
-module.exports = app
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+// module.exports = app
